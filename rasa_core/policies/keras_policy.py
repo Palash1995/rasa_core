317e99652b125ab7bd33557f0f6baa4542a4cd9a
Palash1995 <35071481+Palash1995@users.noreply.github.com>
2019-01-10 13:25:02 +0530

    Update keras_policy.py
    TypeError: fit() got multiple values for keyword argument 'epochs'
    Changing this:
    
        params = self._get_valid_params(self.model.fit, **kwargs)
        self.model.fit(shuffled_X, shuffled_y,
                       epochs=self.epochs, batch_size=self.batch_size,
                       **params)
    to this :
    
        params = self._get_valid_params(self.model.fit, **kwargs)
        self.model.fit(shuffled_X, shuffled_y,**params)
    Then we can pass epochos and batch_size arguments in agent.train()

--- rasa_core/policies/keras_policy.py
+++ rasa_core/policies/keras_policy.py
@@ -156,11 +156,7 @@ class KerasPolicy(Policy):
                                       self.validation_split))
                 # filter out kwargs that cannot be passed to fit
                 params = self._get_valid_params(self.model.fit, **kwargs)
-
-                self.model.fit(shuffled_X, shuffled_y,
-                               epochs=self.epochs,
-                               batch_size=self.batch_size,
-                               **params)
+                self.model.fit(shuffled_X, shuffled_y,**params)
                 # the default parameter for epochs in keras fit is 1
                 self.current_epoch = self.defaults.get("epochs", 1)
                 logger.info("Done fitting keras policy model")
